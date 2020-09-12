31b4a9ad3a01071c9e9362713365c83c077049a0
shohedul350 <shohedul350@gmail.com>
2020-09-12 16:09:27 +0600

    component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Home from './component/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>React App</h1>
+       <Home/>
     </div>
   );
 }
